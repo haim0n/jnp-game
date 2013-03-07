893ea6318924f26f3fa425c551c4e49c65820b97
haim0n <haimdaniel@gmail.com>
2013-03-07 20:30:27 +0200

    PlayScreen implements InputProcessor as well

--- jnp-gdx/src/com/shval/jnpgame/PlayScreen.java
+++ jnp-gdx/src/com/shval/jnpgame/PlayScreen.java
@@ -1,9 +1,10 @@
 package com.shval.jnpgame;
 
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.physics.box2d.World;
 
-public class PlayScreen implements Screen {
+public class PlayScreen implements Screen, InputProcessor {
 
 	private Board board; // this is our world now
 	
@@ -49,6 +50,55 @@ public class PlayScreen implements Screen {
 	public void dispose() {
 		// TODO Auto-generated method stub
 
+	}
+
+	// * InputProcessor methods ***************************//
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
