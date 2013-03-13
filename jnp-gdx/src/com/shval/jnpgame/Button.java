a4da68be0948513e5a2bc9ba03cb644d801b6dc8
haim0n <haimdaniel@gmail.com>
2013-03-14 00:47:20 +0200

    added button

---
+++ jnp-gdx/src/com/shval/jnpgame/Button.java
@@ -0,0 +1,34 @@
+package com.shval.jnpgame;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+
+/*
+TODO: Screen should implement this 
+public interface Command 
+{
+    public void execute(Object data);
+}
+
+public class Button {
+	protected Texture buttonOffTexture; // the button is unpressed
+	protected Texture buttonOnTexture; //  the button is hit
+	protected Screen screen;
+	Runnable actionCB; 					// action callback
+	String text;
+	
+	public Button(String buttonText, Texture onTexture, Texture offTexture, Function actionCB) {
+		this.text = buttonText;
+		this.buttonOnTexture = onTexture;
+		this.buttonOffTexture = offTexture;
+		this.actionCB = actionCB;
+	}
+	
+	public void action() {
+		actionCB();
+	}
+	public void render() {
+		
+	}
+}
+*/
