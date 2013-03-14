6bd4f6a0cf4db93ab0852be0d2c78981c3bfa64e
panatoli <panatoli@gmail.com>
2013-03-14 22:33:25 +0200

    levels 1-6

--- jnp-gdx/src/com/shval/jnpgame/BoardConfig.java
+++ jnp-gdx/src/com/shval/jnpgame/BoardConfig.java
@@ -24,7 +24,7 @@ public class BoardConfig {
 			"xw   yry22   x",
 			"x    rybyB  Yx",
 			"x   Y33Rg    x",
-			"xxxxxxxxxxxxxx"
+			"xxxxxxxxxxxxxx",
 			},
 			{ // level 1			
 			"xxxxxxxxxxxxxx",
@@ -36,7 +36,7 @@ public class BoardConfig {
 			"x      xx    x",
 			"x  g     r b x",
 			"xxbxxxg xxxxxx",
-			"xxxxxxxxxxxxxx"
+			"xxxxxxxxxxxxxx",
 			},
 			{ // level 2
 			"xxxxxxxxxxxxxx",
@@ -46,8 +46,53 @@ public class BoardConfig {
 			"x     g   g  x",
 			"x   r r   r  x",
 			"xxxxx x x xxxx",
-			"xxxxxxxxxxxxxx"
+			"xxxxxxxxxxxxxx",
 			},
+			{ // level 3
+			"xxxxxxxxxxxxxx",
+			"x            x",
+			"x            x",
+			"x   bg  x g  x",
+			"xxx xxxrxxx  x",
+			"x      b     x",
+			"xxx xxxrxxxxxx",
+			"xxxxxxxxxxxxxx",
+			},
+			{ // level 4
+			"xxxxxxxxxxxxxx",
+			"x            x",
+			"x       r    x",
+			"x       b    x",
+			"x       x    x",
+			"x b r        x",
+			"x b r      b x",
+			"xxx x      xxx",
+			"xxxxx xxxxxxxx",
+			"xxxxxxxxxxxxxx",
+			},
+			{ // level 5
+			"xxxxxxxxxxxxxx",
+			"x            x",
+			"x            x",
+			"xrg  gg      x",
+			"xxx xxxx xx  x",
+			"xrg          x",
+			"xxxxx  xx   xx",
+			"xxxxxx xx  xxx",
+			"xxxxxxxxxxxxxx",
+			},
+			{ // level 6
+			"xxxxxxxxxxxxxx",
+			"xxxxxxx      x",
+			"xxxxxxx g    x",
+			"x       xx   x",
+			"x r   b      x",
+			"x x xxx x g  x",
+			"x         x bx",
+			"x       r xxxx",
+			"x   xxxxxxxxxx",
+			"xxxxxxxxxxxxxx",
+			}
 			};
 	
 	
--- jnp-gdx/src/com/shval/jnpgame/JnpGame.java
+++ jnp-gdx/src/com/shval/jnpgame/JnpGame.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Gdx;
 
 public class JnpGame extends Game {
 	private final int logLevel = Application.LOG_DEBUG;
+	private static String TAG = BoardConfig.class.getSimpleName();
 	//private final int logLevel = Application.LOG_ERROR;
 	private int MAX_LEVELS;
 	public BoardConfig config;
@@ -18,7 +19,7 @@ public class JnpGame extends Game {
 		// load assets
 		Assets.load();
 		
-		MAX_LEVELS = 5; // TODO:read from config
+		MAX_LEVELS = 100; // TODO:read from config
 		//
 		Gdx.app.setLogLevel(logLevel);
 		playLevel(currentLevel);
@@ -35,7 +36,11 @@ public class JnpGame extends Game {
 	public void win() {
 		currentLevel++;
 		if (currentLevel <= MAX_LEVELS)
-		playLevel(currentLevel);
+			playLevel(currentLevel);
+		else {
+			Gdx.app.debug(TAG, "You win game. Congradulations!!");
+			// you win
+		}
 	}
 	
 	@Override
