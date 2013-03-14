86124e2e04066580498fcf2edd5345df3119231f
haim0n <haimdaniel@gmail.com>
2013-03-15 00:21:27 +0200

    added button class

--- jnp-gdx/src/com/shval/jnpgame/Button.java
+++ jnp-gdx/src/com/shval/jnpgame/Button.java
@@ -1,31 +1,34 @@
 package com.shval.jnpgame;
 
-/*
-TODO: Screen should implement this 
-public interface Command 
-{
-    public void execute(Object data);
-}
+import java.util.ArrayList;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Button {
-	protected Texture buttonOffTexture; // the button is unpressed
-	protected Texture buttonOnTexture; //  the button is hit
-	protected Screen screen;
-	Runnable actionCB; 					// action callback
-	String text;
+	private ArrayList<Sprite> offSpriteList;
+	private ArrayList<Sprite> onTextureList;
+	private ArrayList<Sprite> currTextureList;
+	private Screen screen;
+	private boolean isPressed;
+	private float onTime; // how long does the button remains pressed, before it pops back 
 	
-	public Button(String buttonText, Texture onTexture, Texture offTexture, Function actionCB) {
-		this.text = buttonText;
-		this.buttonOnTexture = onTexture;
-		this.buttonOffTexture = offTexture;
-		this.actionCB = actionCB;
+	public Button(ArrayList<Sprite> onTextureList, ArrayList<Sprite> offSpriteList, Screen screen, float onTime) {
+		this.onTextureList = onTextureList;
+		this.offSpriteList = offSpriteList;
+		this.currTextureList = offSpriteList;
+		this.screen = screen;
+		isPressed = false;
 	}
 	
-	public void action() {
-		actionCB();
+	public void press() {
+		isPressed = true;
+		currTextureList = onTextureList;
 	}
-	public void render() {
-		
+	
+	public void render(SpriteBatch spriteBatch) {
+		for(Sprite sprite: currTextureList) {
+			sprite.draw(spriteBatch);
+		}
 	}
 }
-*/
